b9b0a77b55d2f152228d5f2a5877a76f5d78a084
Benjamin Sutas <benjamin.sutas@gmail.com>
2023-07-02 15:34:19 +1000

    update to 1.0.3

--- git_tag.ps1
+++ git_tag.ps1
@@ -4,17 +4,18 @@ $version = (Get-Content -Path $versionFilePath -Raw).Trim()
 Write-Host "Attempting to tag $version"
 
 # Add a check to ensure the version file exists and contains a valid version number
-if (-not (Test-Path $versionFilePath) -or (-not $version -as [version])) {
+if (-not (Test-Path $versionFilePath) -or (-not $version -as [version]))
+{
     Write-Host "Error: version.txt file not found or contains an invalid version number."
     exit 1
 }
-
 
 # Check if a Git tag already exists with the specified version
 $tagExistsCommand = "git rev-parse --quiet --verify refs/tags/$version"
 $tagExists = Invoke-Expression -Command $tagExistsCommand
 
-if ($tagExists) {
+if ($tagExists)
+{
     Write-Host "Error: Git tag with version $version already exists."
     exit 1
 }
