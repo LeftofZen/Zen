75fd960bb8286708220f12058286abe2e9bbb74c
Benjamin Sutas <benjamin.sutas@gmail.com>
2023-07-02 13:23:20 +1000

    add Zen.Messaging, defualt editorconfig

---
+++ Zen.Messaging/IMessageStreamDeserialiser.cs
@@ -0,0 +1,7 @@
+﻿namespace Zen.Messaging
+{
+	public interface IMessageStreamDeserialiser<T> where T : IMessage
+	{
+		T? Deserialise(Header hdr, byte[] bytes);
+	}
+}
---
+++ Zen.Messaging/IMessageStreamSerialiser.cs
@@ -0,0 +1,7 @@
+﻿namespace Zen.Messaging
+{
+	public interface IMessageStreamSerialiser<T> where T : IMessage
+	{
+		byte[] Serialise(T msg);
+	}
+}
